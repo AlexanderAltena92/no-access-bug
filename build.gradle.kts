c077dffefb57ef799ba79aa6d392f534355b79f9
Martin Bonnin <martin@mbonnin.net>
2023-12-13 11:41:55 +0100

    use Kotlin 2.0.0-Beta1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
   id("com.android.library") version "8.0.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+  id("org.jetbrains.kotlin.android") version "2.0.0-Beta1" apply false
 
 }
